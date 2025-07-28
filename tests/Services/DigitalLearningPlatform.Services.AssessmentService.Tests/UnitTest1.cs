a27a5f99e5e2e5d8725161ee4d7c708fde9ae6fd
Namasivayam Raja <namasivayamrajamsc@gmail.com>
2025-07-28 18:23:40 +0530

    Update UnitTest1.cs
    Added Dummy Testcases

--- tests/Services/DigitalLearningPlatform.Services.AssessmentService.Tests/UnitTest1.cs
+++ tests/Services/DigitalLearningPlatform.Services.AssessmentService.Tests/UnitTest1.cs
@@ -3,9 +3,9 @@
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void DummyTestPassed()
         {
-
+            Assert.True(true);
         }
     }
 }
